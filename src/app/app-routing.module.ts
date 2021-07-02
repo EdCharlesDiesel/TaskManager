8a9f20f629f77e986e4b1295fc93716d385df55d
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-07-02 13:58:32 +0200

    DEV-03 | Adding dashboard
    I added the dashboard and about components and set up initial routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashBoardComponent },
+  { path: 'about', component: AboutComponent },
+  {path: '', redirectTo: 'dashboard',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
