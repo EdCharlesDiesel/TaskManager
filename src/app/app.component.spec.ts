0eda1337a9f16e38f5237a918b6f4a2d26f332b7
Khotso Mokhethi <Mokhetkc@hotmail.com>
2021-07-02 12:22:50 +0200

    DEV-02
    I build a navigation menu using bootstrap 4 with a search bar and button.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'TaskManager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Task Manager');
+    expect(app.title).toEqual('TaskManager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Task Manager app is running!');
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('Task Manager');
   });
 });
